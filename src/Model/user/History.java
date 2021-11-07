b056e83624d11cbb5372bdf1128dd76c5948eedc
zuocheng li <zuocheng.li@sjsu.edu>
2021-11-07 23:39:33 +0800

    set up History class

---
+++ src/Model/user/History.java
@@ -0,0 +1,20 @@
+package Model.user;
+
+import java.util.ArrayList;
+
+public class History {
+    private String report;
+    private static ArrayList<History> list;
+
+    public History(String report, String data) {
+
+    }
+
+    public static ArrayList<History> getList() {
+        return list;
+    }
+
+    public String getReport() {
+        return report;
+    }
+}
