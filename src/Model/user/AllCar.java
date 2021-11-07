0da5a8d9d41fe6a28bec8d106989db2f0e34e492
zuocheng li <zuocheng.li@sjsu.edu>
2021-11-07 22:55:20 +0800

    set up AllCar class

---
+++ src/Model/user/AllCar.java
@@ -0,0 +1,31 @@
+package Model.user;
+
+import java.util.ArrayList;
+
+public class AllCar {
+    private ArrayList<Model> list;
+
+    public AllCar() {
+
+    }
+
+    public void addModel(Model model) {
+
+    }
+
+    public Model getModel(String name) {
+        return null;
+    }
+
+    public ArrayList<Car> searchByYear(int year) {
+        return null;
+    }
+
+    public Car search(int code) {
+        return null;
+    }
+
+    public ArrayList<Car> searchByTime(Time time) {
+        return null;
+    }
+}
