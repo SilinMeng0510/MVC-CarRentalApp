fc2b8ac892368df3867a44ae07c4dbf0d66daa4e
zuocheng li <zuocheng.li@sjsu.edu>
2021-11-07 23:32:11 +0800

    set up CarReturn class

---
+++ src/Model/user/CarReturn.java
@@ -0,0 +1,27 @@
+package Model.user;
+
+public class CarReturn {
+    private String report;
+    private String data;
+    private CarReservation reservation;
+
+    public CarReturn() {
+
+    }
+
+    public void findReservation(int ID) {
+
+    }
+
+    public void findReservationByName(String name) {
+
+    }
+
+    public void writeReport() {
+
+    }
+
+    public void endReservation() {
+
+    }
+}
